5616db63db38c4920400fa3fe312e10b52449e15
DevrajBhatt <60641403+DevrajBhatt@users.noreply.github.com>
2020-06-25 13:29:29 +0530

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.0.1
+    </div>
+    <strong>Copyright &copy; 2020 DOAT-<?php echo date('Y') ?>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
